a2da0a3cb807e290accd07e7cc120da2815c191e
shkelqim <shkelqim@imbus-peja.de>
2023-03-31 13:46:26 +0200

    two euler problems

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -4,8 +4,8 @@ mod solution;
 
 fn main() {
     println!("Hello there!\n");
-    let problem1: i32 = (1..1000).filter(|&n| n % 3 == 0 || n % 5 == 0).sum();
-    println!("{}", problem1);
+    let problem_1: i32 = (1..1000).filter(|&n| n % 3 == 0 || n % 5 == 0).sum();
+    println!("{}", problem_1);
 
 
     let lazy_list =
@@ -13,5 +13,27 @@ fn main() {
             .map(|(a, _)| a);
     let problem_2 = lazy_list.take_while(|&n| n <= 4000000).filter(|&n| n % 2 == 0).sum::<i32>();
     println!("{}", problem_2);
+
+    fn factors(n: u64) -> Vec<u64> {
+        match (2..=(n as f64).sqrt() as u64).find(|&i| n % i == 0) {
+            Some(i) => {
+                let mut result = factors(n / i);
+                result.push(i);
+                result
+            }
+            None => vec![n],
+        }
+    }
+    let problem_3 = factors(600851475143);
+    println!("{:?}", problem_3.first());
+
+    let problem_4 =
+        (100..=999)
+            .flat_map(|i| (i..=999).map(move |j| i * j))
+            .filter(|n| n.to_string() == n.to_string().chars().rev().collect::<String>())
+            .max()
+            .unwrap_or(0);
+
+    println!("{}", problem_4)
 }
 
--- scala/src/main/scala/Euler.scala
+++ scala/src/main/scala/Euler.scala
@@ -4,22 +4,21 @@ object Euler extends App {
   // --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
 
   private val problem1 = (1 until 1000).view.filter(n => n % 3 == 0 || n % 5 == 0).sum
-  println("problem one: " + problem1)
+  println("problem one: \t" + problem1)
 
   private lazy val lazyList: LazyList[Int] = 0 #:: lazyList.scanLeft(1)(_ + _)
   private val problem2                     = lazyList.takeWhile(_ <= 4000000).filter(_ % 2 == 0).sum
+  println("problem two: \t" + problem2)
 
-  println("problem two: " + problem2)
+  private def factors(n: Long): List[Long] =
+    (2 to math.sqrt(n).toInt).find(n % _ == 0).fold(List(n))(i => i.toLong :: factors(n / i))
+  println("problem three: \t" + factors(600851475143L).last)
 
-  val numbers = List(2, 5, 6)
-  var res     = 1
-  numbers.foreach(x => res *= x)
-  println(res)
+  private val problem4 =
+    (100 to 999).view.flatMap(i => (i to 999).map(_ * i)).filter(n => n.toString == n.toString.reverse).max
 
-  val turing = Map("x" -> "morning", "y" -> "afternoon", "x" -> "evening")
-  println(turing("x"))
+  println("problem four: \t" + problem4)
 
-  def superpower = "coding"
   // --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- --- ---
   println("\n" + "* ** * " * 30)
   private val end = System.nanoTime()
